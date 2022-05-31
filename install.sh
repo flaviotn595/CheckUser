147a24b85b2b97ea4c35b7627a9c0679e11f70dc
DuTra01 <glemyson20@gmail.com>
2022-05-30 22:47:38 -0300

    update

--- install.sh
+++ install.sh
@@ -3,12 +3,16 @@ url='https://github.com/DuTra01/CheckUser'
 cd ~
 
 if ! [ -x "$(command -v git)" ]; then
-    echo 'Error: git is not installed.' >&2
-    exit 1
+    echo 'Erro: git nao esta instalado.' >&2
+    echo 'Instalando Git...'
+
+    sudo apt-get install git -y 1>/dev/null 2>/dev/null
+
+    echo 'Git instalado com sucesso.'
 fi
 
 function install_checkuser() {
-    echo 'Installing CheckUser...'
+    echo 'Instalando CheckUser...'
 
     git clone $url
     cd CheckUser
@@ -25,11 +29,11 @@ function install_checkuser() {
 
 function check_update() {
     if ! [ -d CheckUser ]; then
-        echo 'CheckUser is not installed.'
+        echo 'CheckUser nao esta instalado.'
         exit 1
     fi
 
-    echo 'Checking for updates...'
+    echo 'Verificando atualizacoes...'
     cd CheckUser
 
     git fetch --all
@@ -37,13 +41,20 @@ function check_update() {
     git pull origin master
 
     python3 setup.py install
-    echo 'Update complete.'
+    echo 'CheckUser atualizado com sucesso.'
 }
 
 function uninstall_checkuser() {
-    echo 'Uninstalling CheckUser...'
+    echo 'Desinstalando CheckUser...'
 
     [[ -d CheckUser ]] && rm -rf CheckUser
+
+    [[ -f /usr/bin/checker ]] && {
+        service checker stop
+        /usr/bin/checker --uninstall
+        rm /usr/bin/checker
+    }
+
     [[ -f /usr/local/bin/checker ]] && {
         service checker stop
         /usr/local/bin/checker --remove-service
@@ -53,7 +64,7 @@ function uninstall_checkuser() {
 
 function console_menu() {
     clear
-    echo 'CheckUser Console Menu'
+    echo 'CHECKUSER MENU'
     echo '[01] - Instalar CheckUser'
     echo '[02] - Atualizar CheckUser'
     echo '[03] - Desinstalar CheckUser'
@@ -62,11 +73,27 @@ function console_menu() {
     read -p 'Escolha uma opção: ' option
 
     case $option in
-    01 | 1) install_checkuser ;;
-    02 | 2) check_update ;;
-    03 | 3) uninstall_checkuser ;;
-    00 | 0) exit 0 ;;
-    *) echo 'Opção inválida.' ;;
+    01 | 1)
+        install_checkuser
+        console_menu
+        ;;
+    02 | 2)
+        check_update
+        console_menu
+        ;;
+    03 | 3)
+        uninstall_checkuser
+        console_menu
+        ;;
+    00 | 0)
+        echo 'Saindo...'
+        exit 0
+        ;;
+    *)
+        echo 'Opção inválida.'
+        read -p 'Pressione enter para continuar...'
+        console_menu
+        ;;
     esac
 
 }
